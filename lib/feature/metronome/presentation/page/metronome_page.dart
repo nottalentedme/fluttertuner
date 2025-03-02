0d0e45181be017ad9a09c4df616649edfb940d7e
Denis <routineiseverywhere@gmail.com>
2025-03-02 22:47:24 +0500

    ✅ nav_bar: change to ConvexBottomBar, router fix, svg icons

--- lib/feature/metronome/presentation/page/metronome_page.dart
+++ lib/feature/metronome/presentation/page/metronome_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MetronomePage extends StatelessWidget {
   const MetronomePage({super.key});
 
-  static const path = 'metronome';
+  static const path = '/metronome';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/feature/tuner/presentation/page/tuner_page.dart
+++ lib/feature/tuner/presentation/page/tuner_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class TunerPage extends StatelessWidget {
   const TunerPage({super.key});
 
-  static const path = 'tuner';
+
+  static const path = '/tuner';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Text('TUNER'),),
+    );
   }
 }
