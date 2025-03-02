a2d376c86acba8c756c44e72365dbcaa6e0ac7cc
Denis <routineiseverywhere@gmail.com>
2025-03-02 18:26:49 +0500

    ✅ nav_bar, router: add nav_bar and router

---
+++ lib/feature/tuner/presentation/page/tuner_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TunerPage extends StatelessWidget {
+  const TunerPage({super.key});
+
+  static const path = 'tuner';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
