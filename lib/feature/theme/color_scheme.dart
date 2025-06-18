209fcc0b1e02408f6c9b9dacb85f04956ae950c2
Denis <routineiseverywhere@gmail.com>
2025-06-18 20:53:25 +0500

    💄 colors fix

--- lib/feature/theme/color_scheme.dart
+++ lib/feature/theme/color_scheme.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColorScheme {
   static ColorScheme get light => const ColorScheme.light(
-        primary: Colors.white,
-        onPrimary: Colors.black,
-        secondary: Colors.black,
-        onTertiary: Colors.white,
-        outlineVariant: Colors.black,
-        tertiary: Colors.greenAccent,
-        outline: Colors.black,
-        inversePrimary: Colors.black,
-        onPrimaryFixed: Colors.grey,
-      );
+      primary: Colors.white,
+      onPrimary: Colors.black,
+      secondary: Colors.black,
+      onTertiary: Colors.white,
+      outlineVariant: Colors.black,
+      tertiary: Colors.lightGreen,
+      outline: Colors.black,
+      inversePrimary: Colors.black,
+      onPrimaryFixed: Colors.grey,
+      surfaceTint: Colors.white);
   static ColorScheme get dark => const ColorScheme.dark(
         primary: Color.fromARGB(255, 46, 46, 45),
         onPrimary: Colors.white,
@@ -22,5 +22,6 @@ abstract final class AppColorScheme {
         tertiary: Color.fromARGB(255, 175, 252, 65),
         outline: Color.fromARGB(255, 46, 46, 45),
         inversePrimary: Colors.black,
+        surfaceTint: Color.fromARGB(255, 175, 252, 65),
       );
 }
